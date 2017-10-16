8d39b15cd26a42386ea3b4164a23eb37f4e451ca
Jesse Wynn <wynnsquad@gmail.com>
2017-10-16 14:07:36 -0600

    adding my package

---
+++ scripts/bag_to_video_file.py
@@ -0,0 +1,71 @@
+#! /usr/bin/env python
+
+## Simple ROS node that subscribes to a ROS image topic and writes a .avi video file
+
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
+import math
+import numpy as np
+import os
+
+class BagToVideoWriter(object):
+
+    def __init__(self):
+
+        # set the name and path to video file
+        path = os.path.expanduser('~') + "/Desktop/"
+        filename = rospy.get_param('~filename', 'video') + '.avi'
+
+        # set the desired playback frame rate
+        fps = rospy.get_param('~framerate', 15)
+
+        # set the video size
+        width = rospy.get_param('~img_width', 1288)
+        height = rospy.get_param('~img_height', 964)
+
+        # setup VideoWriter object
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        self.out = cv2.VideoWriter()
+        success = self.out.open(path + filename, fourcc, fps, (width,height), True)
+
+        # initialize CvBridge object
+        self.bridge = CvBridge()
+
+        # setup image subscriber
+        self.image_sub = rospy.Subscriber("/image", Image, self.image_callback)
+
+
+    def image_callback(self, msg):
+
+        # convert ROS image to OpenCV image
+        try:
+            cv_frame = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+        except CvBridgeError as e:
+            print(e)
+
+        # write the frame to file
+        self.out.write(cv_frame)
+
+
+def main():
+    #initialize the node
+    rospy.init_node('bag_to_video_writer')
+
+    #create instance of BagToVideoWriter class
+    writer = BagToVideoWriter()
+
+    #spin
+    try:
+        rospy.spin()
+    except KeyBoardInterrupt:
+        print("Shutting down")
+    #OpenCV cleanup
+    cv2.destroyAllWindows()
+
+    # close the video file
+    writer.out.release()
+
+if __name__ == '__main__':
+    main()
